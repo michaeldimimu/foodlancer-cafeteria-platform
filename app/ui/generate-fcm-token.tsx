60115294c36195f1a153c456119af059267d8350
Michael Salam <michaeldimimu@gmail.com>
2024-12-20 16:18:23 +0100

    Upload fcm token to database after generation

--- app/ui/generate-fcm-token.tsx
+++ app/ui/generate-fcm-token.tsx
@@ -1,10 +1,22 @@
 "use client";
 
 import useFCM from "@/app/utils/hooks/useFCM";
+import axios from "axios";
+import { log } from "console";
+import { useEffect } from "react";
 
 const FCMTokenDisplay = () => {
   const { messages, fcmToken } = useFCM();
   console.log(fcmToken);
+
+  useEffect(() => {
+    if (fcmToken !== null) {
+      axios
+        .post("/api/uploadFCMTokenToDB", { fcmToken })
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    }
+  }, [fcmToken]);
   return null;
 };
 
