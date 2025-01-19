b873f612bd27ed7b700d00334b3863e3a321b7cc
Michael Salam <michaeldimimu@gmail.com>
2025-01-19 19:35:00 +0100

    Update Firebase configuration for new project settings

--- app/utils/firebase/index.ts
+++ app/utils/firebase/index.ts
@@ -4,13 +4,13 @@ import { type FirebaseOptions, initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyBTAcMkbsVJzZh4dNeL-U9k7pLWk5qAQ0Q",
-  authDomain: "fcm-tutorial-2b4ce.firebaseapp.com",
-  projectId: "fcm-tutorial-2b4ce",
-  storageBucket: "fcm-tutorial-2b4ce.firebasestorage.app",
-  messagingSenderId: "1003331905837",
-  appId: "1:1003331905837:web:60f8b6ddb2ed32af734ba4",
-  measurementId: "G-77Z8HEQ6GM",
+  apiKey: "AIzaSyAAZwM-2tlA5YHnLisRbSV5En_QBc2d6_k",
+  authDomain: "foodlancer-ae872.firebaseapp.com",
+  projectId: "foodlancer-ae872",
+  storageBucket: "foodlancer-ae872.firebasestorage.app",
+  messagingSenderId: "123630284354",
+  appId: "1:123630284354:web:92b60161bc7692ba2407db",
+  measurementId: "G-THV2GWYCH1",
 };
 
 const firebaseapp = initializeApp(firebaseConfig);
--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts("https://www.gstatic.com/firebasejs/8.8.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.8.0/firebase-messaging.js");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBTAcMkbsVJzZh4dNeL-U9k7pLWk5qAQ0Q",
-  authDomain: "fcm-tutorial-2b4ce.firebaseapp.com",
-  projectId: "fcm-tutorial-2b4ce",
-  storageBucket: "fcm-tutorial-2b4ce.firebasestorage.app",
-  messagingSenderId: "1003331905837",
-  appId: "1:1003331905837:web:60f8b6ddb2ed32af734ba4",
-  measurementId: "G-77Z8HEQ6GM",
+  apiKey: "AIzaSyAAZwM-2tlA5YHnLisRbSV5En_QBc2d6_k",
+  authDomain: "foodlancer-ae872.firebaseapp.com",
+  projectId: "foodlancer-ae872",
+  storageBucket: "foodlancer-ae872.firebasestorage.app",
+  messagingSenderId: "123630284354",
+  appId: "1:123630284354:web:92b60161bc7692ba2407db",
+  measurementId: "G-THV2GWYCH1",
 };
 
 firebase.initializeApp(firebaseConfig);
