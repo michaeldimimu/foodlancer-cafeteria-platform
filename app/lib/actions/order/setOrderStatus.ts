3fd554a0041e37f9cd097995115bb103ee941a9e
Michael Salam <michaeldimimu@gmail.com>
2025-01-22 05:34:56 +0100

    Refactor order status notification handling to use a single notification object

--- app/lib/actions/order/setOrderStatus.ts
+++ app/lib/actions/order/setOrderStatus.ts
@@ -21,32 +21,35 @@ export default async function setOrderStatus(
     await orderToToggle.save();
 
     // Send a notification to the user about their order's updated status
-    let notificationTitle;
-    let notificationDetails;
+    let notification = { title: "", details: "" };
+
     // Dynamically set the value of the notification title
     value === "preparing"
-      ? (notificationTitle = "Preparing your order...")
+      ? (notification = {
+          title: "Preparing your order...",
+          details:
+            "Your order was successfully received and is now being prepared",
+        })
       : value === "ready"
-        ? (notificationTitle = "Your order is ready for pickup")
+        ? (notification = {
+            title: "Your order is ready for pickup",
+            details: `You can now pick up your order from the cafeteria! Don't forget the Confirmation ID of the order: ${confirmationId}`,
+          })
         : value === "denied"
-          ? (notificationTitle = "Your order has been denied")
-          : (notificationTitle = "Order successfully claimed");
-
-    // Dynamically set the value of the notification details
-    value === "preparing"
-      ? (notificationDetails =
-          "Your order was successfully received and is now being prepared")
-      : value === "ready"
-        ? (notificationDetails = `You can now pick up your order from the cafeteria! Don't forget the Confirmation ID of the order: ${confirmationId}`)
-        : value === "denied"
-          ? (notificationDetails = `We're so sorry, but the cafeteria denied your order: ${message}`)
-          : (notificationDetails = `You have successfully claimed your order. Thank you for using Foodlancer, and enjoy your meal!`);
+          ? (notification = {
+              title: "Your order has been denied",
+              details: `We're so sorry, but the cafeteria denied your order: ${message}`,
+            })
+          : (notification = {
+              title: "Order successfully claimed",
+              details: `You have successfully claimed your order. Thank you for using Foodlancer, and enjoy your meal!`,
+            });
 
     orderToToggle.fcmTokens.forEach(async (fcmToken: string) => {
       await sendNotificationToUser(
         fcmToken,
-        notificationTitle,
-        notificationDetails,
+        notification.title,
+        notification.details,
       );
     });
 
