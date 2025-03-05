c8a03e7610fe0f51aa9afc52af8e7cea1ea134ee
Michael Salam <michaeldimimu@gmail.com>
2025-03-06 00:16:22 +0100

    Change resend sender email from chotenlabs.com to foodlancerapp.com

--- auth/auth.ts
+++ auth/auth.ts
@@ -13,7 +13,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: MongoDBAdapter(client) as Adapter,
   providers: [
     Resend({
-      from: "no-reply@chotenlabs.com",
+      from: "no-reply@foodlancerapp.com",
     }),
   ],
 });
