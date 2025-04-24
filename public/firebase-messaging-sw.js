fe43bc562db9babbcd2afdb4c0101e70a8643a50
Michael Salam <michaeldimimu@gmail.com>
2025-04-24 20:15:25 +0100

    Update env variables

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,6 +2,7 @@ importScripts("https://www.gstatic.com/firebasejs/8.8.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/8.8.0/firebase-messaging.js");
 
 const firebaseConfig = {
+  // PROD
   apiKey: "AIzaSyAAZwM-2tlA5YHnLisRbSV5En_QBc2d6_k",
   authDomain: "foodlancer-ae872.firebaseapp.com",
   projectId: "foodlancer-ae872",
@@ -9,6 +10,15 @@ const firebaseConfig = {
   messagingSenderId: "123630284354",
   appId: "1:123630284354:web:92b60161bc7692ba2407db",
   measurementId: "G-THV2GWYCH1",
+
+  // TEST
+  // apiKey: "AIzaSyBTAcMkbsVJzZh4dNeL-U9k7pLWk5qAQ0Q",
+  // authDomain: "fcm-tutorial-2b4ce.firebaseapp.com",
+  // projectId: "fcm-tutorial-2b4ce",
+  // storageBucket: "fcm-tutorial-2b4ce.firebasestorage.app",
+  // messagingSenderId: "1003331905837",
+  // appId: "1:1003331905837:web:60f8b6ddb2ed32af734ba4",
+  // measurementId: "G-77Z8HEQ6GM",
 };
 
 firebase.initializeApp(firebaseConfig);
